6a8e2efdc582d7c5d2ab2bb69400c66c503fba22
franco contreras <shibateemo@gmail.com>
2022-08-11 17:09:31 -0300

    agregue funcion ternaria y sweets alerts

--- jefe.js
+++ jefe.js
@@ -3,13 +3,14 @@
 //         pregunta: "https://drive.google.com/file/d/1IzuNFBNSyKTbfA84bZZMDLmIAfa56uq4/view",
 //         respuestaNo: "https://drive.google.com/file/d/1YGJtfJZBCbdmkTpdvVnhmbQhNTqXotxx/view",
 //         respuestaSi: "https://drive.google.com/file/d/1nR4Y7A8Xa7K8sk8TU-yi-cbQgYXU_3e8/view"
-    
+
 //     }
 // ]
 
+
 const alerts = ['<h3>si ha cometido un delito y quiere confesarlo , pulse si , si no es asi pulse no</h3>']
 
-let contQ = document.querySelector(".contQ").innerHTML=(alerts[0])
+let contQ = document.querySelector(".contQ").innerHTML = (alerts[0])
 
 let burbujaNo = document.createElement("p");
 burbujaNo.innerHTML = "<h3> ha pulsado no, osea que si ha cometido un delito, pero no quiere confesarlo un auto patrulla esta yendo para su casa</h3>"
@@ -22,38 +23,42 @@ console.log(divJefe)
 const botonYes = document.querySelector("#yes")
 
 
+
 const botonNo = document.querySelector("#no")
 
 
-botonNo.onclick =() =>{document.querySelector(".jefe").innerHTML = "<h4> ha pulsado no, osea que si ha cometido un delito, pero no quiere confesarlo un auto patrulla esta yendo para su casa</h4>"}
+botonNo.onclick = () => { document.querySelector(".jefe").innerHTML = "<h4> ha pulsado no, osea que si ha cometido un delito, pero no quiere confesarlo un auto patrulla esta yendo para su casa</h4>" ;
+Swal.fire(' ha pulsado no, osea que si ha cometido un delito, pero no quiere confesarlo un auto patrulla esta yendo para su casa');}
 
-botonYes.onclick =() =>{document.querySelector(".jefe").innerHTML = "<h4> estamos mandando un movil a su ubicacion</h4>" 
-}
+botonYes.onclick = () => {
+    document.querySelector(".jefe").innerHTML = "<h4> estamos mandando un movil a su ubicacion</h4>"
+;Swal.fire('estamos mandando un movil a su ubicacion'); }
 
 
 console.log(botonNo);
 console.log(botonYes);
 
 
-let contCar =document.querySelector(".contCar").innerHTML = localStorage.getItem('auto')
+let contCar = document.querySelector(".contCar").innerHTML = localStorage.getItem('auto')
 console.log(contCar)
 
 
-localStorage.setItem('auto','<img class="car" src="multimedia/auto.png" alt="patrulla">')
+localStorage.setItem('auto', '<img class="car" src="multimedia/auto.png" alt="patrulla">')
 
 const jefeDialogos = {
     dialogo1: 'estamos todos arrestados',
-    dialogo2: 'y mientras espera , puede comprar una gorra , una camiseta , tiene derecho a permanecer fabuloso'
+    dialogo2: 'y mientras espera , puede comprar una gorra , una camiseta , tiene derecho a permanecer fabuloso',
+    dialogo3: 'bienvenido a la policia web de springfield',
 }
 
 
-const enJson =JSON.stringify(jefeDialogos)
+const enJson = JSON.stringify(jefeDialogos)
 
 console.log(typeof enJson)
 
 localStorage.setItem("producto1", enJson)
 
-const dialogosJson =JSON.parse(enJson)
+const dialogosJson = JSON.parse(enJson)
 
 console.log(typeof enJson)
 
@@ -63,3 +68,15 @@ let remeras = document.querySelector(".remeras").innerHTML = ('<h4> y mientras e
 // console.log(typeof enJson)
 
 // localStorage.setItem("producto1", enJson)
+
+
+
+/*---------------------------------------------------------------------------------
+--------------------------- FUNCION TERNARIA---------------------------------------
+-----------------------------------------------------------------------------------*/
+
+const jefeRemeras = (jefeDialogos.dialogo3 ='bienvenido a la policia web de springfield') ? true :false
+
+jefeRemeras ?Swal.fire('bienvenido a la policia web de springfield') : Swal.fire(' ha pulsado no, osea que si ha cometido un delito, pero no quiere confesarlo un auto patrulla esta yendo para su casa');Swal.fire('y mientras espera , puede comprar una gorra , una camiseta , tiene derecho a permanecer fabuloso')
+
+
